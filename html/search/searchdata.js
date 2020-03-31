a081bf7588ffac8f42eeb32986f64f29c0ac2be2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-31 17:29:15 +0000

    Deploy neosensory/NeosensoryBluefruit to github.com/neosensory/NeosensoryBluefruit.git:gh-pages

--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,18 +1,27 @@
 var indexSectionsWithContent =
 {
-  0: "n",
-  1: "n"
+  0: "abcdfgimnrstv",
+  1: "n",
+  2: "abcdfgimnrstv",
+  3: "mn",
+  4: "n"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "pages"
+  1: "classes",
+  2: "functions",
+  3: "variables",
+  4: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Pages"
+  1: "Classes",
+  2: "Functions",
+  3: "Variables",
+  4: "Pages"
 };
 
